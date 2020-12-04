da0c58e0e22e536b73bc3c414510999960e820d4
joaoguzman <joaogg@ug.uchile.cl>
2020-12-04 16:09:34 -0300

    errores

--- clase_bodega.py
+++ clase_bodega.py
@@ -6,29 +6,27 @@ diccionario_flores = {"lilium":"l", "rosa":"r", "gardenia":"g", "alheli":"a", "b
 especies_flores = ["a", "b", "j", "l", "r", "g", "f", "t","c"]
 tamaño_flores = ["S", "L"]
 
-class Archivo:
-    def __init__(self,nombre):
-        self.__nombre_archivo = nombre +".txt"
-        self.__crear_archivo()
-    
-    def __crear_archivo(self):
-        with open(self.__nombre_archivo, 'a') as file:
-            file.write("Inventario Iniciado")
-    
-    def agregar_elemento(self, elemento):
-        with open(self.__nombre_archivo, 'a') as file:
-            file.write("\n"+elemento)
-
-
-
-
-class Bodega (Archivo):
-    def __init__(self, __nombre_archivo):
-        Archivo.__init__(self, __nombre_archivo)
+class Bodega ():
+    def __init__(self): #inicializa la clase con un argumento nombre de archivo
+        self.__nombre_archivo = "bodega_actualizada.dat"
         self.lista_bodega = []
         self.bodega_sistematizada = {}
         self.bodega_pickle = []
-    
+        self.__crear_archivo()
+
+    def __crear_archivo(self):
+        if Path(self.__nombre_archivo).is_file():
+            print ("File exist")
+        else:
+            data = {}
+            with open(self.__nombre_archivo, 'wb') as file:
+                pickle.dump(data, file)
+                
+    def recibir_diccionario(self, dic):
+        self.bodega_sistematizada = dic
+        with(open("bodega_actualizada.dat", "wb")) as pf:
+                pickle.dump(self.bodega_sistematizada, pf)
+        
     def recibir_flores(self, numero_flores_recibir):
         for numero in range(0,numero_flores_recibir):
             especie_escogida = random.choice(especies_flores)
@@ -46,14 +44,14 @@ class Bodega (Archivo):
         if Path("bodega_actualizada.dat").is_file():
             print ("File exist")
             with (open("bodega_actualizada.dat", "rb")) as pf:
-                self.pickle_abierto = pickle.load(pf)
+                pickle_abierto = pickle.load(pf)
             for clave, elemento in self.bodega_sistematizada.items():
-                if clave in self.pickle_abierto.keys():
-                    self.pickle_abierto[clave] += elemento
+                if clave in pickle_abierto.keys():
+                    pickle_abierto[clave] += elemento
                 else:
-                    self.pickle_abierto[clave] = elemento
+                    pickle_abierto[clave] = elemento
             with(open("bodega_actualizada.dat", "wb")) as pf:
-                pickle.dump(self.pickle_abierto, pf)
+                pickle.dump(pickle_abierto, pf)
         else:
             with(open("bodega_actualizada.dat", "wb")) as pf:
                 pickle.dump(self.bodega_sistematizada, pf)
@@ -72,26 +70,3 @@ class Bodega (Archivo):
                 except EOFError:
                     break
             return self.bodega_sistematizada
-            #print(self.bodega_pickle)
-
-#funcion generar flores (sin guardarlas: flor generada + lista de flores iniciales)
-#actualizar diccionario: te entrega diccionario tal como lo pase o con modificaciones: se guarda en archivo diccionario
-#archivo registro de flores: llegan flores una por una 
-#archivo de diccionario (se sobre escribe)
-
-#FUNCION QUE SOBRE ESCRIBA EL ARCHIVO ORIGINAL 
-'''
-bodega1 = Bodega("bodega.txt")
-x = bodega1.recibir_flores(100)
-#bodega1.listar_bodega()
-bodega1.sistematizacion_bodega()
-print(bodega1.bodega_sistematizada)
-bodega1.actualizar_archivo()
-bodega1.abrir_pickle()
-'''
-#generar lista de flores
-#transformo a diccionario
-#se guarda en dat
-#lo leo y le agrego las flores generadas
-#dos archivos: uno de lista de flores generadas
-#siempre vamos a trabajar con archivo de diccionario
